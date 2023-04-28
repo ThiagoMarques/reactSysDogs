58002c3594d7b1fe8d8d4af3f42469edaa07f7da
ThiagoMarques <thiagossmarques@gmail.com>
2023-04-27 21:27:05 -0300

    feat: adiciona componentes header e footer, adiciona rotas do react

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import Api from './api/Api';
 import './App.css'
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './Components/Login/Login';
 
 const App = () => {
-  return <div><Api /></div>;
+  return (
+  <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Api />
+        <Footer />
+      </BrowserRouter>
+
+    </div>
+    );
 };
 
 export default App;
