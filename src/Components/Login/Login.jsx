a700495383ccd311d40c19b6165544d22eca0765
ThiagoMarques <thiagossmarques@gmail.com>
2023-04-27 21:48:21 -0300

    feat: adiciona rotas para login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import LoginForm from './LoginForm'
+import LoginCreate from './LoginCreate'
+import LoginPasswordLost from './LoginPasswordLost'
+import LoginPasswordReset from './LoginPasswordReset'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+      <Routes>
+        <Route path="/" elemnt={<LoginForm />} />
+        <Route path="/criar" elemnt={<LoginCreate />} />
+        <Route path="/perdeu" elemnt={<LoginPasswordLost />} />
+        <Route path="/resetar" elemnt={<LoginPasswordReset />} />
+      </Routes>
+    </div>
   )
 }
 
